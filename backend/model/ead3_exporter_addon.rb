3197af75f7652fbbf577d03f76088999c859d5ca
Mark Custer <fordmadox@gmail.com>
2020-05-05 15:17:23 -0400

    on second thought, we will add daoset back to the mix

--- backend/model/ead3_exporter_addon.rb
+++ backend/model/ead3_exporter_addon.rb
@@ -340,7 +340,9 @@ class EAD3Serializer < EADSerializer
    return if digital_object["publish"] === false && !@include_unpublished
    return if digital_object["suppressed"] === true
 
-   file_versions = digital_object['file_versions']
+   # ANW-285: Only serialize file versions that are published, unless include_unpublished flag is set
+   file_versions_to_display = digital_object['file_versions'].select {|fv| fv['publish'] == true || @include_unpublished }
+
    title = digital_object['title']
    date = digital_object['dates'][0] || {}
 
@@ -358,9 +360,9 @@ class EAD3Serializer < EADSerializer
      end
    end
 
-   #the title is already added to daodesc/p, so no need to repeat here.
-   atts['linktitle'] = digital_object['digital_object_id'] if digital_object['digital_object_id']
-   # and let's keep those URIs everywhere.... can use this during the post-export process to group daos into daoset elements
+   #the title is already added to descriptivenote/p, so no need to repeat here. let's use the new identifier attribute in EAD3
+   atts['identifier'] = digital_object['digital_object_id'] if digital_object['digital_object_id']
+   # and let's keep those URIs in the same place everywhere...
    atts['altrender'] = digital_object['uri']
 
    if digital_object['digital_object_type']
@@ -370,25 +372,39 @@ class EAD3Serializer < EADSerializer
      atts['daotype'] = 'unknown'
    end
 
-   if file_versions.empty?
-     atts['href'] = digital_object['digital_object_id']
-     atts['actuate'] = 'onrequest'
-     atts['show'] = 'new'
+   if file_versions_to_display.empty?
      atts['audience'] = 'internal' unless is_digital_object_published?(digital_object)
      xml.dao(atts) {
-       xml.descriptivenote { sanitize_mixed_content(content, xml, fragments, true) } if content
+       xml.descriptivenote{ sanitize_mixed_content(content, xml, fragments, true) } if content
+     }
+   elsif file_versions_to_display.length == 1
+     file_version = file_versions_to_display.first
+     atts['actuate'] = file_version['xlink_actuate_attribute'] || 'onrequest'
+     atts['show'] = file_version['xlink_show_attribute'] || 'new'
+     atts['role'] = file_version['use_statement'] if file_version['use_statement']
+     atts['title'] = file_version['caption'] if file_version['caption']
+     atts['href'] = file_version['file_uri']
+     atts['audience'] = 'internal' unless is_digital_object_published?(digital_object, file_version)
+     xml.dao(atts) {
+       xml.descriptivenote{ sanitize_mixed_content(content, xml, fragments, true) } if content
      }
    else
-     file_versions.each do |file_version|
-       atts['href'] = file_version['file_uri'] || digital_object['digital_object_id']
-       atts['actuate'] = (file_version['xlink_actuate_attribute'].respond_to?(:downcase) && file_version['xlink_actuate_attribute'].downcase) || 'onrequest'
-       atts['show'] = (file_version['xlink_show_attribute'].respond_to?(:downcase) && file_version['xlink_show_attribute'].downcase) || 'new'
-       atts['localtype'] = file_version['use_statement'] if file_version['use_statement']
-       atts['audience'] = 'internal' unless is_digital_object_published?(digital_object, file_version)
-       xml.dao(atts) {
-         xml.descriptivenote { sanitize_mixed_content(content, xml, fragments, true) } if content
-       }
-     end
+     set_atts = {}
+     set_atts['altrender'] = atts['altrender']
+     set_atts['audience'] = 'internal' unless is_digital_object_published?(digital_object)
+     atts.delete('altrender')
+     xml.daoset( set_atts ) {
+       file_versions_to_display.each do |file_version|
+         atts['actuate'] = file_version['xlink_actuate_attribute'] || 'onrequest'
+         atts['show'] = file_version['xlink_show_attribute'] || 'new'
+         atts['role'] = file_version['use_statement'] if file_version['use_statement']
+         atts['title'] = file_version['caption'] if file_version['caption']
+         atts['href'] = file_version['file_uri']
+         atts['audience'] = 'internal' unless is_digital_object_published?(digital_object, file_version)
+         xml.dao( atts )
+       end
+       xml.descriptivenote{ sanitize_mixed_content(content, xml, fragments, true) } if content
+     }
    end
  end
 
